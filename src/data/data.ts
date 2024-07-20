254e7c8a3624e5de7d2e308461ae4de69be8c12f
eloka-95 <emeholisaeloka@gmail.com>
2024-07-20 18:21:34 +0100

    adding display image

--- src/data/data.ts
+++ src/data/data.ts
@@ -37,25 +37,25 @@ export const HomeData = [
   {
     title: 'Section 1',
     data: [
-      { id: '1', name: 'Item 1', details: 'Details 1', price: '₦ 10', image: require('../assets/images/Rectangle70.png'), icon: require('../assets/images/icons/listicon.png'), dotsIcon:require('../assets/images/icons/dots.png'), naira:require('../assets/images/icons/naira.png') },
+      { id: '1', name: 'Noodles', details: 'Indomie', price: '₦ 10', image: require('../assets/images/Rectangle78.png'), icon: require('../assets/images/icons/listicon.png'), dotsIcon:require('../assets/images/icons/dots.png'), naira:require('../assets/images/icons/naira.png') },
     ],
   },
   {
     title: 'Section 2',
     data: [
-      { id: '2', name: 'Item 2', details: 'Details 2', price: '₦ 10', image: require('../assets/images/Rectangle76.png'), icon: require('../assets/images/icons/listicon.png'), dotsIcon:require('../assets/images/icons/dots.png') , naira:require('../assets/images/icons/naira.png')},
+      { id: '2', name: 'Rice', details: 'Brewers rice', price: '₦ 10', image: require('../assets/images/Rectangle76.png'), icon: require('../assets/images/icons/listicon.png'), dotsIcon:require('../assets/images/icons/dots.png') , naira:require('../assets/images/icons/naira.png')},
     ],
   },
   {
     title: 'Section 2',
     data: [
-      { id: '2', name: 'Item 2', details: 'Details 2', price: '₦ 10', image: require('../assets/images/Rectangle76.png'), icon: require('../assets/images/icons/listicon.png'), dotsIcon:require('../assets/images/icons/dots.png') , naira:require('../assets/images/icons/naira.png')},
+      { id: '2', name: 'Spaghetti', details: 'Golden Penny', price: '₦ 10', image: require('../assets/images/Rectangle70.png'), icon: require('../assets/images/icons/listicon.png'), dotsIcon:require('../assets/images/icons/dots.png') , naira:require('../assets/images/icons/naira.png')},
     ],
   },
   {
     title: 'Section 2',
     data: [
-      { id: '2', name: 'Item 2', details: 'Details 2', price: '₦ 10', image: require('../assets/images/Rectangle76.png'), icon: require('../assets/images/icons/listicon.png'), dotsIcon:require('../assets/images/icons/dots.png') , naira:require('../assets/images/icons/naira.png')},
+      { id: '2', name: 'Toothpaste', details: 'Multiple brands', price: '₦ 10', image: require('../assets/images/Rectangle80.png'), icon: require('../assets/images/icons/listicon.png'), dotsIcon:require('../assets/images/icons/dots.png') , naira:require('../assets/images/icons/naira.png')},
     ],
   },
 ];
